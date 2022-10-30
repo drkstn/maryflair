0d94220f94b3e3a4d51343e551f51ec728422eea
drkstn <86159096+drkstn@users.noreply.github.com>
2022-10-30 06:17:10 -0700

    Add default loader/redirect to auth/google route

--- app/routes/auth.google.js
+++ app/routes/auth.google.js
@@ -1,5 +1,8 @@
 import { authenticator } from "../services/auth.server";
 import { SocialsProvider } from "remix-auth-socials";
+import { redirect } from "@remix-run/node";
+
+export let loader = () => redirect("/");
 
 export const action = async ({ request }) => {
   // initiating authentication using Google Strategy
