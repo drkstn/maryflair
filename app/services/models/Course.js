56afdf72750f692448f20229ff91a85fb56adaa5
drkstn <86159096+drkstn@users.noreply.github.com>
2022-11-11 12:08:13 -0800

    Add mongoose-slug-generator and nanoid dependencies

--- app/services/models/Course.js
+++ app/services/models/Course.js
@@ -1,8 +1,13 @@
 import { mongoose } from "~/services/db.server";
+import slugGenerator from "mongoose-slug-generator/lib/slug-generator";
+import { nanoid } from "nanoid";
+mongoose.plugin(slugGenerator);
 
 const CourseSchema = new mongoose.Schema({
+  nanoid: { type: String, unique: true, default: nanoid(10) },
   owner: String,
   name: String,
+  slug: { type: String, slug: "name" },
   objective: String,
   notes: [String],
   lessons: [{ type: mongoose.Schema.Types.ObjectId, ref: "Lesson" }],
